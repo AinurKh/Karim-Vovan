5f7eebcf15cd191851f4f29f8818705e9271f99a
AinurKh <ainur14.khaybulov@gmail.com>
2025-05-15 11:11:25 +0300

    first commit

---
+++ src/main/java/com/example/karimvovan/KarimVovanApplication.java
@@ -0,0 +1,13 @@
+package com.example.karimvovan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarimVovanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarimVovanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/karimvovan/KarimVovanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.karimvovan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarimVovanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
