6e6f18f1f4bbdf077955c68d4f2bba4ed1731c40
Карим <kbulatov01@gmail.com>
2025-06-05 01:54:39 +0300

    Отполировал чотбы работало)

--- src/main/resources/db/migration/V2__create_related_tables.sql
+++ src/main/resources/db/migration/V2__create_related_tables.sql
@@ -1,11 +1,11 @@
-CREATE TABLE books(
+CREATE TABLE books (
     id SERIAL PRIMARY KEY,
-    title VARCHAR(100)NOT NULL,
+    title VARCHAR(100) NOT NULL,
     author_id INT NOT NULL,
     publication_year INT
 );
 
-CREATE Table book_details(
+CREATE TABLE book_details (
     id SERIAL PRIMARY KEY,
     book_id INT NOT NULL,
     isbn VARCHAR(13) NOT NULL,
@@ -13,22 +13,25 @@ CREATE Table book_details(
     CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books(id),
     CONSTRAINT unique_book_id UNIQUE (book_id)
 );
-CREATE TABLE authors(
+
+CREATE TABLE authors (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     birth_year INT
 );
-CREATE TABLE genres(
+
+CREATE TABLE genres (
     id SERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL
 );
+
 CREATE TABLE book_genres (
-    book INT NOT NULL,
+    book_id INT NOT NULL,
     genre_id INT NOT NULL,
-    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books(id)),
+    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books(id),
     CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres(id),
     CONSTRAINT fk_book_genres PRIMARY KEY (book_id, genre_id)
 );
 
 ALTER TABLE books
-ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors(id);
+    ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors(id);
