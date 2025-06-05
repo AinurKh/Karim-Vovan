312452d0bf43072f535eef4cb531ebdc3ecb8541
Карим <kbulatov01@gmail.com>
2025-06-06 00:42:35 +0300

    Заполнил класс Genre

--- src/main/java/com/example/karimvovan/entity/Genre.java
+++ src/main/java/com/example/karimvovan/entity/Genre.java
@@ -1,4 +1,31 @@
 package com.example.karimvovan.entity;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "genres")
 public class Genre {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer Id;
+
+    @Column(length = 50, nullable = false)
+    String name;
+
+    //геттеры и сеттеры
+
+
+    public Integer getId() {
+        return Id;
+    }
+    public void setId(Integer id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
 }
