91bbb16ae55274755ee636b9c9008e5e9f00cecf
Карим <kbulatov01@gmail.com>
2025-06-06 01:22:39 +0300

    Заполнил интерфейс GenreRepository

--- src/main/java/com/example/karimvovan/repository/GenreRepository.java
+++ src/main/java/com/example/karimvovan/repository/GenreRepository.java
@@ -1,4 +1,7 @@
 package com.example.karimvovan.repository;
 
-public class GenreRepository {
+import com.example.karimvovan.entity.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GenreRepository extends JpaRepository<Genre, Long> {
 }
