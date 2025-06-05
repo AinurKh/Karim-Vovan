eb262bf7fd9b9f54f7e1e5fec25bed68295cf086
Karim Bulatov <84225208+karimBulatov@users.noreply.github.com>
2025-06-05 08:47:46 +0300

    Feature/task3 study flyway (#6)
    * подключил Flyway
    
    * создал независимые таблицы
    
    * создал связанные таблицы
    
    * Заполнил таблицы
    
    * Отполировал чотбы работало)

---
+++ src/main/resources/db/migration/V2__create_related_tables.sql
@@ -0,0 +1,37 @@
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    author_id INT NOT NULL,
+    publication_year INT
+);
+
+CREATE TABLE book_details (
+    id SERIAL PRIMARY KEY,
+    book_id INT NOT NULL,
+    isbn VARCHAR(13) NOT NULL,
+    page_count INT,
+    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books(id),
+    CONSTRAINT unique_book_id UNIQUE (book_id)
+);
+
+CREATE TABLE authors (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    birth_year INT
+);
+
+CREATE TABLE genres (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE book_genres (
+    book_id INT NOT NULL,
+    genre_id INT NOT NULL,
+    CONSTRAINT fk_book FOREIGN KEY (book_id) REFERENCES books(id),
+    CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres(id),
+    CONSTRAINT fk_book_genres PRIMARY KEY (book_id, genre_id)
+);
+
+ALTER TABLE books
+    ADD CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors(id);
