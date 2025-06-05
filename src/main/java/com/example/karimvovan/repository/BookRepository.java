696cd4040cff9bdc8e90a03d19080c3c5cfe677e
Карим <kbulatov01@gmail.com>
2025-06-06 01:22:30 +0300

    Заполнил интерфейс BookRepository

--- src/main/java/com/example/karimvovan/repository/BookRepository.java
+++ src/main/java/com/example/karimvovan/repository/BookRepository.java
@@ -1,4 +1,7 @@
 package com.example.karimvovan.repository;
 
-public class BookRepository {
+import com.example.karimvovan.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
